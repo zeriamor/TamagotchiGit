26a75289b7d5ef9974c91ace34f01e32452376ad
Ángela Amor <angamoram@alu.edu.gva.es>
2026-01-03 19:11:33 +0100

    Commit inicial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//TIP Para <b>ejecutar</b> el código, pulsar <shortcut actionId="Run"/> o
+// Haz clic en el ícono <icon src="AllIcons.Actions.Execute"/> del margen.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Git");
+
+    }
+}
