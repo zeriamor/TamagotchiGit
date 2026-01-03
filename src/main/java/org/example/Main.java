708ac20672f1e465eb11269e4f002feae5321f25
Ángela Amor <angamoram@alu.edu.gva.es>
2026-01-03 21:09:01 +0100

    Creado menuPrincipal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP Para <b>ejecutar</b> el código, pulsar <shortcut actionId="Run"/> o
 // Haz clic en el ícono <icon src="AllIcons.Actions.Execute"/> del margen.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Git");
+        System.out.println("Probando menuPrincipal():");
+        int opcion = menuPrincipal();
+        System.out.println("El usuario eligió: " + opcion);
 
+    }
+    private static int menuPrincipal(){
+        Scanner tecl=new Scanner(System.in);
+        int opcion;//Opcion convertida a numero
+        while(true){
+            System.out.println("1. Estado");
+            System.out.println("2. Comer");
+            System.out.println("3. Jugar");
+            System.out.println("4. Dormir");
+            System.out.println("---");
+            System.out.println("0. Salir");
+            System.out.println("Selecciona una opción del menú:");
+            try{
+                opcion=Integer.parseInt(tecl.nextLine());//Conversión a numero
+                //Validar 0,1,2,3 o 4.
+                if (opcion>=0 && opcion<=4){
+                    return opcion;//Solo devuelve opciones válidas.
+                }else{
+                    System.out.println("Opción no válida, intente de nuevo.");
+                }
+            }catch (NumberFormatException e){// NumberFormatException es el tipo específico de error (formato numérico inválido)
+                System.out.println("Entrada inválida, debe ser un numero.");
+            }
+        }
     }
 }
